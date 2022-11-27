b5962028d7598a49c3529dcdfc9a386a8943916d
XEMES <cyrilbaudoin@gmail.com>
2022-11-27 01:51:59 +0100

    MAJ Spring TODO optimiser le script JS d'affichage HTML

---
+++ src/main/java/com/example/ademo3/Ademo3Application.java
@@ -0,0 +1,13 @@
+package com.example.ademo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ademo3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ademo3Application.class, args);
+    }
+
+}
